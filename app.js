634ca4156398267925bd4b8858f071325b4fc187
Thiago Nunes Batista <67487679+thiagonunesbatista@users.noreply.github.com>
2023-02-02 10:48:44 -0300

    add sample methods and  attack method

---
+++ app.js
@@ -0,0 +1,36 @@
+Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100
+    }
+  },
+  watch: {
+    monsterHealth(newHealthValue, oldHealthValue) {
+      const hasMonsterBeenAttacked = newHealthValue - oldHealthValue
+
+      if (hasMonsterBeenAttacked) {
+        this.makeMonsterAttack()
+      }
+    }
+  },
+  methods: {
+    makeMonsterAttack() {
+      this.makeAttack('playerHealth', 8, 15)
+    },
+    makeAttack(enemyHealthKey, minimumAttackDamage, maximumAttackDamage) {
+      const randomAttackDamage =
+        Math.floor(
+          Math.random() * (maximumAttackDamage - minimumAttackDamage)
+        ) + minimumAttackDamage
+
+      this[enemyHealthKey] -= randomAttackDamage
+    },
+    makePlayerAttack() {
+      this.makeAttack('monsterHealth', 5, 12)
+    },
+    makePlayerSpecialAttack() {},
+    healPlayer() {},
+    handleSurrender() {}
+  }
+}).mount('#game')
